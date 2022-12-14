2fd5d37b1c0fd0f1f906deffe4fe932403d2eb4e
izuhara2 <izuhara0221@gmail.com>
2022-12-14 17:02:11 +0900

    main

--- js/mati-gyouretu.js
+++ js/mati-gyouretu.js
@@ -4,7 +4,7 @@ let ALF = 1; //流れ密度
 let DEL = 4; //平均処理時間
 let SIG = .5; //処理時間のばらつき
 let EPS = .000001; //log計算時のバイアス
-let a = new Array(20);
+let regi = new Array(20);
 let b = new Array(200);
 let ans = 0.0;
 let time = 0.0;
@@ -14,7 +14,7 @@ function main() {
 
     //console.log("試行回数   平均待ち時間");
     for (i = 0; i < 20; i++) {
-        a[i] = 0.0;
+        regi[i] = 0.0;
     }
     for (i = 0; i < 200; i++) {
         b[i] = 0.0;
@@ -24,9 +24,9 @@ function main() {
         time += poison(time);
         //console.log( {time} );
         console.log({time,a});
-        ans = tim(ans, time, a, b);
-        cll(time, a);
-        sentaku(time, a, b);
+        ans = tim(ans, time, regi, b);
+        cll(time, regi);
+        sentaku(time, regi, b);
 
         
 
@@ -49,40 +49,40 @@ function gauss() {
 }
 
 //空き窓口ルーチン
-function cll(time, a) {
+function cll(time, regi) {
     for (let j = 0; j < M; j++) {        //jは窓口数と比較
-        if (a[j] < time) {
+        if (regi[j] < time) {
             console.log("窓口" +j+ "が空いています");
-            a[j] = 0.0;
+            regi[j] = 0.0;
             return;
         }
     }
 }
 
 //最短空き窓口
-function minimum(a) {
+function minimum(regi) {
     let min = M;
-    a[min] = 100000.;
+regia[min] = 100000.;
     for (let p = 0; p < M; p++) {
-        if (a[min] > a[p])
+        if (regi[min] > regi[p])
             min = p;
     }
     return min;
 }
 
 //待ち解除ルーチン
-function tim(ans, time, a, b) {
+function tim(ans, time, regi, b) {
     for (let j = 0; j < k; j++) {
-        let min = minimum(a);
+        let min = minimum(regi);
 
-        let dummy_a2 = a[min];
+        let dummy_a2 = regi[min];
         //console.log( {a, dummy_a2, time });
-        if (a[min] < time) {
-            ans += a[min] - b[0];
-            let dummy_a = a[min];
+        if (regi[min] < time) {
+            ans += regi[min] - b[0];
+            let dummy_a = regi[min];
             let dummy_b = b[0];
             //console.log( {ans, dummy_a, dummy_b } ); //デバック用
-            a[min] += gauss();
+            regi[min] += gauss();
             k--;
             let l;
             for (l = 0; l < k; l++) {
@@ -95,13 +95,13 @@ function tim(ans, time, a, b) {
 }
 
 //窓口選択ルーチン
-function sentaku(t, a, b) {
+function sentaku(t, regi, b) {
     for (let j = 0; j < M; j++) {
-        if (a[j] == 0.0) {
+        if (regi[j] == 0.0) {
             ///let gg = gauss();
             //console.log( {j, gg, t}); //デバック用
             console.log(j + "に入りました");
-            a[j] = t + gauss();
+            regi[j] = t + gauss();
             return;
         }
     }
@@ -115,4 +115,37 @@ function poison(t) {
     return taw;
 }
 
+/* アニメーション部 */
+const anime = document.querySelector('#anime');
+const madogui = anime.getContext('2d');
+madogui.strokeRect(60,220,80,80);//窓口0
+madogui.strokeRect(170,220,80,80);//窓口1
+madogui.strokeRect(280,220,80,80);//窓口2
+madogui.strokeRect(390,220,80,80);//窓口3
+madogui.strokeRect(500,220,80,80);//窓口4
+madogui.strokeRect(610,220,80,80);//窓口5
+madogui.strokeRect(720,220,80,80);//窓口6
+madogui.strokeRect(830,220,80,80);//窓口7
+madogui.strokeRect(940,220,80,80);//窓口8
+madogui.strokeRect(1050,220,80,80);//窓口9
+
+// window.onload = ()=>{
+//     // canvas準備
+//     const board = document.querySelector("#anime");  //getElementById()等でも可。オブジェクトが取れれば良い。
+//     const ctx = board.getContext("2d");
+  
+//     // 画像読み込み
+//     const customer = new Image();
+//     customer.src = "/img/customer_woman.jpg";  // 画像のURLを指定
+//     customer.onload = () => {
+//       ctx.drawImage(customer, 610, 0, 120, 80);
+//     };
+//   };
+
+ /*スタートボタン押された時の処理*/
+str.addEventListener("click", function () { 
+     
+  })
+
 main();
+
